9a319678eae5ebc58490c06c137ad8d522edb4f3
Hafizhun Soekiswo <apple@Hafizhuns-MacBook-Pro-2.local>
2024-09-26 13:56:23 +0700

    fix: figuring out the bug on mobile

--- script.js
+++ script.js
@@ -66,7 +66,9 @@ Array.from(draggables).forEach(draggable => {
 
     // Touch events
     draggable.addEventListener('touchstart', function (e) {
-        e.preventDefault(); // Prevent touch scroll
+        if (hasDragged || (currentX !== startX || currentY !== startY)) {
+            e.preventDefault(); // Prevent touch scroll
+        }
         const touch = e.touches[0];
         startDrag(touch.clientX, touch.clientY);
 
