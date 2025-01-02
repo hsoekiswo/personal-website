249673ab6c337be64c20f71dccf1ccf94fba1d81
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-01-03 06:52:37 +0700

    fix: public key

--- build.js
+++ build.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 // Replace `process.env` placeholders in the template file
 const content = `
 window.env = {
-    PUBLIC_KEY: "${process.env.API_KEY}",
+    PUBLIC_KEY: "${process.env.PUBLIC_KEY}",
     SERVICE_ID: "${process.env.SERVICE_ID}",
     TEMPLATE_ID: "${process.env.TEMPLATE_ID}"
 };
 `;
 
-fs.writeFileSync('./env.js', content);
+fs.writeFileSync('./public/env.js', content);
 console.log('Environment variables injected into env.js');
